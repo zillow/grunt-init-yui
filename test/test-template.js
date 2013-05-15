6f7953734796db30943c7a95ea063e6d1e490356
Daniel Stockman <daniels@zillow.com>
2013-05-15 14:01:23 -0700

    Actually run init cycle during unit test.

--- test/test-template.js
+++ test/test-template.js
@@ -3,6 +3,7 @@
 // built-ins
 var fs = require('fs');
 var path = require('path');
+var spawn = require('child_process').spawn;
 
 // devDependencies
 var mkdirp = require('mkdirp');
@@ -13,8 +14,12 @@ var template = require('../template');
 
 // shared variables
 var OUTPUT_DIR = __dirname + '/template-output';
+var TEMPLATE   = path.resolve(__dirname, '..'); // grunt-init <path/to/template>
+var GRUNT_INIT = path.join(TEMPLATE, 'node_modules/grunt-init/bin/grunt-init');
 
-describe("template", function () {
+describe("templating", function () {
+    this.timeout(0);
+
     before(function (done) {
         mkdirp(OUTPUT_DIR, done);
     });
@@ -23,5 +28,35 @@ describe("template", function () {
         rimraf(OUTPUT_DIR, done);
     });
 
-    it("TODO");
+    describe("module", function () {
+        it("should create project if missing", function (done) {
+            var child = spawn(GRUNT_INIT, [TEMPLATE], {
+                cwd: OUTPUT_DIR,
+                stdio: 'pipe'
+            });
+
+            child.stdout.setEncoding('utf8');
+            child.stdout.on('data', function (data) {
+                // start of a prompt "[?]"
+                if (data.indexOf('[') === 0) {
+                    // send newline to accept defaults
+                    process.nextTick(function () {
+                        child.stdin.write('\n');
+                    });
+                }
+            });
+
+            child.on('close', function (code) {
+                done();
+            });
+        });
+
+        it("should not overwrite existing project");
+
+        it("should not overwrite existing module");
+    });
+
+    describe("project only", function () {
+        it("should not output module content");
+    });
 });
